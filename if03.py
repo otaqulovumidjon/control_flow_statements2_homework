1090363d3eb8ec6ae232f519d6bd97f8994a954f
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-08 12:07:58 +0500

    if03

--- if03.py
+++ if03.py
@@ -8,4 +8,16 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+
+    if b>a and a>c or c>a and a>b:
+        x = 'First number'
+
+    if a>b and b>c or c>b and b>a:
+        x = 'Second number'
+    
+    if a>c and c>b or b>c and c>a:
+        x = 'Third number'
+
+    return x
+
+print(main(2, 4, 6))
