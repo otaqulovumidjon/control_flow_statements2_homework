c531e73667001c248b41ae5bcb82f61cd3b6a4c7
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-08 23:43:49 +0500

    if06

--- if06.py
+++ if06.py
@@ -17,14 +17,14 @@ def main(n):
     n5 = n%10
 
     if n1>=n2 and n1>=n3 and n1>=n4 and n1>=n5:
-        return n1, '0'
+        return 1
     if n2>=n1 and n2>=n3 and n2>=n4 and n2>=n5:
-        return n2, '1'
+        return 2
     if n3>=n1 and n3>=n2 and n3>=n4 and n3>=n5:
-        return n3, '2'
+        return 3
     if n4>=n1 and n4>=n1 and n4>=n1 and n4>=n5:
-        return n4, '3'
+        return 4
     if n5>=n1 and n5>=n2 and n5>=n3 and n5>=n4:
-        return n5, '4'
+        return 5
 
 print(main(87394))
