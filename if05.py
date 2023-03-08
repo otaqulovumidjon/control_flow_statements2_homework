ee051369fc046914811cd4b70801ca32ab6f5346
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-08 18:48:21 +0500

    if05

--- if05.py
+++ if05.py
@@ -11,19 +11,15 @@ def main(n):
     n3 = n//100%10
     n4 = n//10%10
     n5 = n%10
-    if n1>=n2 and n1>=n3 and n1>=n4 and n1>=n5:
-        return n1
-
-    if n2>=n1 and n2>=n3 and n2>=n4 and n2>=n5:
-        return n2
-
-    if n3>=n1 and n3>=n2 and n3>=n4 and n3>=n5:
-        return n3
-
-    if n4>=n1 and n4>=n2 and n4>=n3 and n4>=n5:
-        return n4
-
-    if n5>=n1 and n5>=n2 and n5>=n3 and n5>=n4:
-        return n5
+    max = n1
+    if n2>max:
+        max=n2
+    if n3>max:
+        max=x3
+    if n4>max:
+        max=n4
+    if n5>max:
+        max=n5
+    return max
 
 print(main(89349))
