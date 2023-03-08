778a77c7dd9f8251b15b24cce2b1f724bb878500
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-08 23:03:11 +0500

    if07

--- if06.py
+++ if06.py
@@ -6,4 +6,25 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    n1 = n%10
+    n//10
+    n2 = n%10
+    n//10
+    n3 = n%10
+    n//10
+    n4 = n%10
+    n//10
+    n5 = n%10
+
+    max = n1
+    if n2 > max:
+        max = n2
+    if n3 > max:
+        max = n3
+    if n4 > max:
+        max = n4
+    if n5 > max:
+        max = n5
+    return max
+    
+print(main(13579))
